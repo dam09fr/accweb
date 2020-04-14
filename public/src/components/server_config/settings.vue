358d7aa3005218251c6392b0ee6b4d883fb58a66
Damien GUILLEM <contact@dguillem.fr>
2020-04-14 21:36:48 +0200

    ACC Server password to clear text
    I think this is more simple for configuration to see clear text passwords.

--- public/src/components/server_config/settings.vue
+++ public/src/components/server_config/settings.vue
@@ -1,12 +1,12 @@
 <template>
     <collapsible :title="$t('title')">
         <field :label="$t('servername_label')" v-model="serverName"></field>
-        <field type="password" :label="$t('password_label')" v-model="password"></field>
-        <field type="password" :label="$t('adminpassword_label')" v-model="adminPassword"></field>
+        <field type="text" :label="$t('password_label')" v-model="password"></field>
+        <field type="text" :label="$t('adminpassword_label')" v-model="adminPassword"></field>
         <field type="number" :label="$t('trackmedalsrequirement_label')" v-model="trackMedalsRequirement"></field>
         <field type="number" :label="$t('safetyratingrequirement_label')" v-model="safetyRatingRequirement"></field>
         <field type="number" :label="$t('racecraftratingrequirement_label')" v-model="racecraftRatingRequirement"></field>
-        <field type="password" :label="$t('spectatorpassword_label')" v-model="spectatorPassword"></field>
+        <field type="text" :label="$t('spectatorpassword_label')" v-model="spectatorPassword"></field>
         <field type="number" :label="$t('dumpleaderboards_label')" v-model="dumpLeaderboards"></field>
         <field type="number" :label="$t('isracelocked_label')" v-model="isRaceLocked"></field>
         <field type="number" :label="$t('randomizetrackwhenempty_label')" v-model="randomizeTrackWhenEmpty"></field>
